7c7ca2049680e13ceeb280c2da86667c527a6ad0
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-01-16 20:42:49 +0300

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Vasichko Anna on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
